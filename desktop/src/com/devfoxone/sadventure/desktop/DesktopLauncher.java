5545c0c4ac02a08abe2c6213f0fb579449fbe00b
FWI-95 <devfoxone@gmail.com>
2021-04-29 21:41:06 +0200

    initial commit

---
+++ desktop/src/com/devfoxone/sadventure/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.devfoxone.sadventure.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.devfoxone.sadventure.ShinyAdventure;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ShinyAdventure(), config);
+	}
+}
